7a6698d7d4664b641897cfb1501070435cac2ad3
mdjonestwo <mdjones2@protonmail.com>
2021-02-06 14:33:40 -0500

    added get request to retrieve workouts with duration and weight totals & updates stat page

--- 02-Homework/Develop/routes/apiRoutes.js
+++ 02-Homework/Develop/routes/apiRoutes.js
@@ -6,9 +6,16 @@ mongoose.set("useFindAndModify", false);
 
 module.exports = (app) => {
   app.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
+    db.Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: "$exercises.duration" },
+          totalWeight: { $sum: "$exercises.weight" },
+        },
+      },
+    ])
       .then((response) => {
-        res.json(response);
+        console.log(response), res.json(response);
       })
       .catch((err) => {
         res.json(err);
@@ -40,11 +47,35 @@ module.exports = (app) => {
       });
   });
 
-  /*
   app.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({}).then((response) => {
-      res.json(response);
-    });
+    db.Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: "$exercises.duration" },
+          totalWeight: { $sum: "$exercises.weight" },
+        },
+      },
+    ])
+      .sort({ _id: -1 })
+      .limit(7)
+      .then((response) => {
+        res.json(response);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
   });
-  */
+
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findOneAndUpdate(
+      { _id: req.params.id },
+      { $push: { exercises: [req.body] } }
+    )
+      .then((response) => {
+        res.json(response);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 };
